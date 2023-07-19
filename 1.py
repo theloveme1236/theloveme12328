a341fef71425df8b5fe489e5901f33baf1a86ba8
theloveme1236 <139608310+theloveme1236@users.noreply.github.com>
2023-07-19 12:35:40 -0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -36,18 +36,18 @@ def like3like_login():
         if cookies_totel_1=='like':
             print(cookies_totel)
             driver.get("https://www.like4like.org/#social-media-platform-list")
-            cookies = pickle.load(open(cookies_totel, "rb"))
+            cookies = pickle.load(open('{}'.format(cookies_totel), "rb"))
             for cookie in cookies:
                 driver.add_cookie(cookie)
-   
 for cookies_totel in os.listdir(os.getcwd()):
     cookies_totel_1 = cookies_totel.split('_cookies')[0]
-    if cookies_totel_1=='like':        
-        driver.get("https://www.like4like.org/#social-media-platform-list")
-        cookies = pickle.load(open(cookies_totel, "rb"))
+    if cookies_totel_1=='like':
         print(cookies_totel)
+        driver.get("https://www.like4like.org/#social-media-platform-list")
+        cookies = pickle.load(open('{}'.format(cookies_totel), "rb"))
         for cookie in cookies:
             driver.add_cookie(cookie)
+        
     if cookies_totel_1=='youtube':
         driver.get("https://www.youtube.com/")
         print(cookies_totel)
