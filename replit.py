39c4942e7bfc87e0871fe05ad2a0dcd4cf868692
theloveme1236 <139608310+theloveme1236@users.noreply.github.com>
2023-07-23 22:56:18 -0800

    commit message

--- replit.py
+++ replit.py
@@ -76,10 +76,10 @@ def Subscribe():
             driver.find_element(By.XPATH, '/html/body/div[6]/div/div[1]/div[2]/div[2]/div[4]/div[1]/div[2]/div[1]/div/div[3]/div/div').click()
             time.sleep(5)
             driver.switch_to.window(driver.window_handles[1])
-            time.sleep(5)
+            time.sleep(2)
             driver.find_element(By.ID, 'subscribe-button').click()
             
-            time.sleep(5)
+            time.sleep(3)
             #driver.save_screenshot('{}.png'.format(s))
             driver.close()
             driver.switch_to.window(driver.window_handles[0])
